2cb31f159bd63131dc974e48d015e2738ab1053a
fegonzalez7 <fegonzalezro@unal.edu.co>
2025-11-23 10:47:45 -0500

    chore update main version

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from app.api.routers.health import router as health_router
 from app.core.lifespan import lifespan
 
 
-app = FastAPI(title="docs-rag-ms", version="0.0.1", lifespan=lifespan)
+app = FastAPI(title="docs-rag-ms", version="0.0.2", lifespan=lifespan)
 
 app.include_router(health_router)
 app.include_router(api_router)
