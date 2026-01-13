96befdcc002824dc2746e70c058733dfdf169c25
fegonzalez7 <fegonzalezro@unal.edu.co>
2026-01-12 19:59:55 -0500

    feat: add vertex ai support to the infrastucture layer

--- backend/app/infrastructure/llm/client.py
+++ backend/app/infrastructure/llm/client.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+from langchain_google_vertexai import ChatVertexAI
 from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
 from pydantic import SecretStr
@@ -5,11 +8,14 @@ from pydantic import SecretStr
 from app.core.config import Settings
 
 
+LLMClientType = Union[ChatOpenAI, ChatOllama, ChatVertexAI]
+
+
 class LLMClient:
     """LLM client wrapper."""
 
     def __init__(self, settings: Settings) -> None:
-        self._client: ChatOpenAI | ChatOllama
+        self._client: LLMClientType
         self._llm_model: str
 
         if settings.local_llm:
@@ -17,23 +23,35 @@ class LLMClient:
                 model=settings.ollama_model,
                 reasoning=settings.ollama_thinking,
                 base_url=settings.ollama_base_url,
-                temperature=settings.openai_temperature,
-                top_p=settings.openai_top_p,
+                temperature=settings.llm_temperature,
+                top_p=settings.llm_top_p,
             )
             self._llm_model = settings.ollama_model
 
         else:
+            if settings.use_vertex_ai:
+                self._client = ChatVertexAI(
+                    model=settings.vertex_ai_model,
+                    project=settings.vertex_ai_project,
+                    location=settings.vertex_ai_location,
+                    temperature=settings.llm_temperature,
+                    max_tokens=settings.llm_max_tokens,
+                    top_p=settings.llm_top_p,
+                )
+                self._llm_model = settings.vertex_ai_model
+                return
+
             self._client = ChatOpenAI(
                 model=settings.openai_model,  # type: ignore[call-arg]
                 api_key=SecretStr(settings.openai_api_key),
-                temperature=settings.openai_temperature,
-                max_tokens=settings.openai_max_tokens,
-                top_p=settings.openai_top_p,
+                temperature=settings.llm_temperature,
+                max_tokens=settings.llm_max_tokens,
+                top_p=settings.llm_top_p,
             )
             self._llm_model = self._client.model_name
 
     @property
-    def client(self) -> ChatOpenAI | ChatOllama:
+    def client(self) -> LLMClientType:
         return self._client
 
     @property
